539fa37e210231f76e47f60a30286ba035586ab6
Himanshu817324 <himanshupandey9378@gmail.com>
2025-09-18 11:23:45 +0530

    cors

--- server/simple-server.js
+++ server/simple-server.js
@@ -16,7 +16,7 @@ app.use(cors({
     process.env.FRONTEND_URL || 'http://localhost:3000',
     'http://localhost:5173',
     'http://localhost:5174',
-    'https://your-netlify-site.netlify.app'
+    'https://mindshieldai.netlify.app'
   ],
   credentials: true
 }));
